6c28e86422d555ba495355108dad2274e3ab2e6e
Chanuth Senviru <70003702@learning.acbt.lk>
2025-05-21 15:51:36 +1000

    Edited Css

--- styles/apply.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tech Careers Inc. - Job Application</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
