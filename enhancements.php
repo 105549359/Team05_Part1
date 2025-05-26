d0430c0998b164440a7559e837e4f33205a4f9ee
105507535 <105507535@student.swin.edu.au>
2025-05-26 23:01:39 +1000

    Main Section and Hero Banner

--- enhancements.php
+++ enhancements.php
@@ -6,3 +6,11 @@ include('header.inc');
 <div class="container">
     <?php include('nav.inc'); ?>
 </div>
+
+<main>
+    <section class="hero">
+        <div class="container">
+            <h2>Website Enhancements</h2>
+            <p>Additional features and improvements implemented in this project.</p>
+        </div>
+    </section>
